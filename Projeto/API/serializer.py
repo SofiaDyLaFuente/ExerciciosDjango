7333f1c489cdf26f58036042704b4892a553bfa6
Sofia Dy La Fuente Monteiro <150842264+SofiaDyLaFuente@users.noreply.github.com>
2025-01-03 10:12:33 -0300

    correção de bug

--- Projeto/API/serializer.py
+++ Projeto/API/serializer.py
@@ -4,37 +4,41 @@ from rest_framework.exceptions import ValidationError
 
 class TarefaSerializer(serializers.ModelSerializer):
 
-    extra_kwargs = {
-            'email': {'write_only':True}
-        }
+    class Meta:
     
-    model = Tarefa
-    
-    fields = (
-            'tipo',
-            'titulo',
-            'data_criacao',
-            'data_conclusao',
-            'descricao',
-            'prioridade',   
-    )
-
-    def validate_data_conclusao(self, value):
-        if value and value < self.instance.data_criacao.date():
-            raise ValidationError("A data de conclusão não pode ser anterior a data da criação")
+        extra_kwargs = {
+                'email': {'write_only':True}
+            }
         
-        return value
+        model = Tarefa
+        
+        fields = (
+                'tipo',
+                'titulo',
+                'data_criacao',
+                'data_conclusao',
+                'descricao',
+                'prioridade',   
+        )
+
+        def validate_data_conclusao(self, value):
+            if value and value < self.instance.data_criacao.date():
+                raise ValidationError("A data de conclusão não pode ser anterior a data da criação")
+            
+            return value
 
 class ListaSerializer(serializers.ModelSerializer):
 
-    tarefas = TarefaSerializer(many = True, read_only = True)
-    model = Lista
-    fields = ('nome','quadro','tarefas')
+    class Meta:
+        tarefas = TarefaSerializer(many = True, read_only = True)
+        model = Lista
+        fields = ('nome','quadro','tarefas')
 
 
 class QuadroSerializer(serializers.ModelSerializer):
 
-    listas = ListaSerializer(many = True, read_only = True)
-    model = Quadro
-    fields = ('nome', 'listas')
+    class Meta:
+        listas = ListaSerializer(many = True, read_only = True)
+        model = Quadro
+        fields = ('nome', 'listas')
 
